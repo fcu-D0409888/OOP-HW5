329506ea73bb233e21490cbd932c1b17b3714cc9
user <user@x450j-nb>
2016-11-28 14:22:28 +0800

    HW5

---
+++ src/fcu/iecs/oop/pokemon/GYM.java
@@ -0,0 +1,20 @@
+package fcu.iecs.oop.pokemon;
+
+public class GYM {
+	public static void fight(Pokemon p1, Pokemon p2){
+		int winner,cp = 0;
+		winner = (int)(Math.random()*2)+1;
+		if(winner == 1){
+			System.out.printf("Winner is %s",p1.getName());
+			cp = p1.getCp() + 50;
+			p1.setCp(cp);
+			System.out.println(p1.getCp());
+		}
+		else{
+			System.out.printf("Winner is %s",p2.getName());
+			cp = p2.getCp() + 50;
+			p2.setCp(cp);
+		}
+	}
+
+}
